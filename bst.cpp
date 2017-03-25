0ec63135430517561197494deb29c9e8063d6d02
Harish Palani <harishpalani3@gmail.com>
2017-03-25 14:55:44 +0000

    insert & search functions completed; deletion still incomplete

--- bst.cpp
+++ bst.cpp
@@ -25,22 +25,52 @@ int main() {
     }
 }
 
+// Create a new BST node
 BSTNode* create(int data) {
+    BSTNode* node = new BSTNode();
     
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+    
+    return node;
 }
 
+// Insert a node into the BST
 BSTNode* insert(BSTNode* root, int data) {
+    if (root == NULL) { // If null, create a new node
+        root = create(data);
+    } else if (data <= root->data) { // Else, insert data into the proper location on the tree
+        root->left = insert(root->left, data);
+    } else { 
+        root->right = insert(root->right, data);
+    }
     
+    return root;
 }
 
+// Search for a node in the BST
 bool search(BSTNode* root, int data) {
-    
+    if (root = NULL) {
+        return false;
+    } else if (data == root->data) {
+        return true;
+    } else if (data <= root->data) {
+        return search(root->left, data);
+    } else {
+        return search(root->right, data);
+    }
 }
 
+// Find the smallest node in BST
 BSTNode* findMin(BSTNode* root) {
-    
+    while (root->left != NULL) {
+        root = root->left;
+    }
+    return root;
 }
 
+// Delete a node from the BST
 BSTNode* remove(BSTNode* root, int data) {
     
 }
