f9454e75fc51a5dc563d1a7e85b4d6f13e1433e9
Harish Palani <harishpalani3@gmail.com>
2017-03-25 18:02:53 +0000

    Project finalized & commented

--- bst.cpp
+++ bst.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
-#include <iomanip>
+#include <sstream>
+#include <stdio.h>
+#include <vector>
 
 using namespace std;
 
 struct BSTNode {
     int data;
-    BSTNode *left;
-    BSTNode *right;
+    BSTNode* left;
+    BSTNode* right;
 };
 
 // Function prototypes
@@ -15,67 +17,65 @@ BSTNode* insert(BSTNode* root, int data);
 bool search(BSTNode* root, int data);
 BSTNode* findMin(BSTNode* root);
 BSTNode* remove(BSTNode* root, int data);
-BSTNode* visualize(BSTNode* root, int indent);
+void visualize(BSTNode* root, int spaces);
 
 int main() {
-    while (true) {
-        BSTNode* root = NULL;
-        
-        // Get input through console
-        char numbers[128];
-        cout << "Enter the list:" << endl;
-        cin.ignore();
-        cin.getline(numbers, 128);
-        
-        int i = 0;
-        while (numbers[i]) {
-            if (isdigit(numbers[i])) {
-                int num = numbers[i] - 48;
-                
-                // Construct the entire number from input
-                while (isdigit(numbers[++i])) {
-                    num = 10*num + (numbers[i] - 48);
-                }
-                
-                insert(root, num);
-            } else {
-                i++;
-            }
-        }
-        
-        while (true) {
-            char response;
-            cout << "\nChoose an option: \n1 | Print out tree\n2 | Insert a number\n3 | Remove a number\n4 | Create a new list" << endl;
-            cin >> response;
-            
-            if (response == '1') {
-                visualize(root, 0);
-            } else if (response == '2') {
-                int n;
-                cout << "Number to insert: ";
-                cin >> n;
-                cout << endl;
-                insert(root, n);
-                visualize(root, 0);
-            } else if (response == '3') {
-                int n;
-                cout << "Number to delete: ";
-                cin >> n;
-                cout << endl;
-                if (remove(root, n)) { visualize(root, 0); }
-                else { cout << "Sorry! Number could not be found." << endl; }
-            } else {
-                break;
-            }
+    cout << "Binary Search Tree" << "\n" << "Created by Harish Palani, March 2017" << "\n" << endl;
+    BSTNode* root = NULL;
+    
+    // Get input list
+    cout << "Enter your list, separated by spaces:" << endl;
+    string input;
+    getline(cin, input);
+    stringstream ss(input);
+    
+    // Load input list into vector
+    vector<int> list;
+    int i;
+    while (ss >> i) {
+        list.push_back(i);
+        if (ss.peek() == ' ') {
+            ss.ignore();
         }
     }
+    
+    // Insert vector contents into BST
+    for (i = 0; i < list.size(); i++) {
+        root = insert(root, list.at(i));
+    }
+    
+    while (true) {
+        int n;
+        char response;
+        cout << "\n1 | Print out tree\n2 | Insert a number\n3 | Remove a number\nq | quit" << endl;
+        cin >> response;
+        
+        if (response == '1') {
+            visualize(root, 0);
+        } else if (response == '2') {
+            cout << "Number to insert: ";
+            cin >> n;
+            cout << endl;
+            insert(root, n);
+            visualize(root, 0);
+        } else if (response == '3') {
+            cout << "Number to delete: ";
+            cin >> n;
+            cout << endl;
+            if (remove(root, n)) { visualize(root, 0); }
+            else { cout << "Sorry! Number could not be found." << endl; }
+        } else {
+            break;
+        }
+    }
+    
     return 0;
 }
 
 // Create a new BST node
 BSTNode* create(int data) {
     BSTNode* node = new BSTNode();
-    
+
     node->data = data;
     node->left = NULL;
     node->right = NULL;
@@ -89,7 +89,7 @@ BSTNode* insert(BSTNode* root, int data) {
         root = create(data);
     } else if (data <= root->data) { // Else, insert data into the proper location on the tree
         root->left = insert(root->left, data);
-    } else { 
+    } else {
         root->right = insert(root->right, data);
     }
     
@@ -98,7 +98,7 @@ BSTNode* insert(BSTNode* root, int data) {
 
 // Search for a node in the BST
 bool search(BSTNode* root, int data) {
-    if (root = NULL) { // If null, there's nothing to search
+    if (root == NULL) { // If null, there's nothing to search
         return false;
     } else if (data == root->data) { // If current node has the desired value, return true
         return true;
@@ -120,50 +120,48 @@ BSTNode* findMin(BSTNode* root) {
 
 // Delete a node from the BST
 BSTNode* remove(BSTNode* root, int data) {
-    if (root = NULL) { // If null, there's nothing to delete
+    if (root == NULL) { // If null, there's nothing to delete
         return root;
     } else if (data < root->data) { // Move left or right depending on the desired value
         root->left = remove(root->left, data);
     } else if (data > root->data) {
         root->right = remove(root->right, data);
     } else { // data == root->data
-        if ((root->left == NULL) && (root->right == NULL)) { // If surrounded by null, delete
+        if (root->left == NULL && root->right == NULL) { // If surrounded by null, delete
             delete root;
             root = NULL;
         } else if (root->left == NULL) { // If only left is null, move root to the right & delete
-            BSTNode* node = root;
+            BSTNode* temp = root;
             root = root->right;
-            delete node;
+            delete temp;
         } else if (root->right == NULL) { // If only right is null, move root to the left & delete
-            BSTNode* node = root;
+            BSTNode* temp = root;
             root = root->left;
-            delete node;
+            delete temp;
         } else {
             BSTNode* min = findMin(root->right);
             root->data = min->data;
             root->right = remove(root->right, min->data);
         }
     }
-    
     return root;
 }
 
-// Visualize BST
-BSTNode* visualize(BSTNode* root, int indent) {
-    if (root != NULL) {
-        if(root->right) {
-            visualize(root->right, indent + 4);
-        }
-        if (indent) {
-            cout << setw(indent) << ' ';
-        }
-        if (root->right) {
-            cout<<" /\n" << setw(indent) << ' ';
-        }
-        std::cout<< root->data << "\n ";
-        if (root->left) {
-            cout << setw(indent) << ' ' << " \\\n";
-            visualize(root->left, indent+4);
-        }
-    }
+// Visualize BST (sideways tree)
+void visualize(BSTNode* root, int spaces) {
+    if (root == NULL) { return; }
+ 
+    // Set distance between levels
+    int c = 3;
+    spaces += c;
+ 
+    // Right child
+    visualize(root->right, spaces);
+ 
+    // Print space + current node
+    for (int i = c; i < spaces; i++) { printf(" "); }
+    printf("%d\n", root->data);
+ 
+    // Left child
+    visualize(root->left, spaces);
 }
